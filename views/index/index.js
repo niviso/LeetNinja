db5848fd72380c016816cc7f26c077bde4e4f595
Nikki Sollid <nikki.sollid@gmail.com>
2020-02-04 11:24:53 +0100

    first commit

---
+++ views/index/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import styles from "./style.scss";
+export default function Index() {
+  return (
+    <View style={Styles.container}>
+      <Text>Open up Index.js to start working on your Index!</Text>
+    </View>
+  );
+}
+
