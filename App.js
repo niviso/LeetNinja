366dfcd46114c8650622841f9fff0a8753651167
Nikki Sollid <nikki.sollid@gmail.com>
2020-02-05 12:38:49 +0100

    programming the jump

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Index from './views/index/index';
 import { GameProvider } from "./Contexts/GameContext";
+import {View,Dimensions,TouchableWithoutFeedback,Text} from 'react-native';
 
 export default function App() {
+  const screenWidth = Math.round(Dimensions.get('window').width);
+  const screenHeight = Math.round(Dimensions.get('window').height);
   return (
     <GameProvider>
-      <Index/>
+      <Index screenHeight={screenHeight}/>
     </GameProvider>
   );
 }
--- views/index/index.js
+++ views/index/index.js
@@ -2,20 +2,33 @@ import React,{useState,useEffect} from 'react';
 import {View,Dimensions,TouchableWithoutFeedback,Text} from 'react-native';
 import styles from "./style.scss";
 
-export default function Index() {
+export default function Index(props) {
   const [directionVector,setDirectionVector] = useState([0,0]);
-  const [charPosition,setCharPosition] = useState(0);
+  const [charPositionX,setCharPositionX] = useState(0);
+  const [charPositionY,setCharPositionY] = useState(1);
+  const {screenHeight} = props;
+
   const speed = 5;
   var Loop = null;
   const fps = 60;
+  var onGround = false;
+  var gravity = 2.3;
+
 
  useEffect(() => {
   const interval = setInterval(() => {
      // Your custom logic here
-     setCharPosition(charPosition => charPosition + (directionVector[0] * speed));
+     setCharPositionX(charPositionX => charPositionX + (directionVector[0] * speed));
+     setCharPositionY(charPositionY => charPositionY + directionVector[1]);
+     setDirectionVector(directionVector => [directionVector[0],directionVector[1]-gravity])
+     if(charPositionY >= (screenHeight-100)){
+      setCharPositionY(screenHeight - 100);
+     }
+    
+
    }, (1000/fps));
   return () => clearInterval(interval);
-}, [charPosition,directionVector]);
+}, [charPositionX,charPositionY,directionVector]);
 
   return (
     <View style={styles.container}>
@@ -32,10 +45,12 @@ export default function Index() {
     <View style={styles.stats}>
         <Text style={styles.headline}>[{directionVector.toString()}]</Text>
         <Text style={styles.headline}>FPS: {fps}</Text>
+        <Text style={styles.headline}>FPS: {screenHeight}</Text>
+
 
 </View>
-        <View style={{...styles.character,left: charPosition}}>
-        <Text>{charPosition}</Text>
+        <View style={{...styles.character,left: charPositionX,top: charPositionY}}>
+        <Text>{Math.round(charPositionX) + "," + Math.round(charPositionY)}</Text>
         </View>
     </View>
   );
