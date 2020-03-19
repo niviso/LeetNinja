607215fb84b8918a1c4f03616efb698ff6f6b286
freenewipads2 <nikki.sollid@gmail.com>
2020-03-19 09:12:51 +0100

    Added music and credits

--- views/start/start.js
+++ views/start/start.js
@@ -1,15 +1,16 @@
 import React,{useContext,useEffect} from 'react';
 import { GameContext } from "../../Contexts/GameContext";
-import {View,Image,Text} from 'react-native';
+import {View,Image,Text,TouchableOpacity} from 'react-native';
 import Background from '../../components/background/background';
 import AudioHelper from '../../helpers/AudioHelper'
 
 export default function Start(props) {
   const {screenHeight,screenWidth,setScreen} = props;
   const [state,setState] = useContext(GameContext);
-  AudioHelper.play(require('../../assets/sound/menu.mp3'),true);
+  AudioHelper.play(require('../../assets/sound/downloaded/tropical_loop.wav'),true);
 
   return (
+    <TouchableOpacity onPress={() => setScreen('game')}>
     <View style={{
       width: screenWidth,
       height: screenHeight,
@@ -25,6 +26,7 @@ export default function Start(props) {
     <Text>Start Game</Text>
     </View>
     </View>
+    </TouchableOpacity>
 
   );
 }
